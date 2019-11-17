9320d00c46af90eb04366b3f8e2948a7cd6a6ac7
sulliman abdul <sulliman.abdul@gmail.com>
2019-11-17 03:05:41 +0100

    ajout des scripts de save

---
+++ MedicalGameJam/Assets/Scripts/GameSaving/AnswerSaving.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnswerSaving : MonoBehaviour
+{
+    public static AnswerSaving answerSaving;
+    private static string jsonSavePath = "";
+
+    public string sceneName;
+    public string question;
+    public string answer;
+    public int answerCount;
+    public int totalCount;
+
+
+    public static string lSceneName;
+    public static string lQuestion;
+    public static string lAnswer;
+    public static int lAnswerCount;
+    public static int lTotalCount;
+
+    public static bool isLoaded = false;
+
+    private void Awake()
+    {
+        if (answerSaving == null)
+        {
+           answerSaving = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public static void LoadData()
+    {
+        GameSaving gameSaving = JsonUtility.FromJson<GameSaving>(File.ReadAllText(Application.persistentDataPath + "/saveload.json"));
+
+        GameSaving.lSceneName = gameSaving.sceneName;
+        GameSaving.lScore = gameSaving.score;
+
+        SceneManager.LoadScene(gameSaving.sceneName);
+
+        //For testing purposes
+
+
+        GameSaving.isLoaded = true;
+    }
+
+    public static void SaveData(string question, string answer)
+    {
+        //References
+        Scene scene = SceneManager.GetActiveScene();
+        AnswerSaving answerSaving = new AnswerSaving();
+        //Scene Name
+        AnswerSaving.lTotalCount += 1;
+        answerSaving.sceneName = scene.name;
+
+        //Position
+
+        AnswerSaving.lSceneName = answerSaving.sceneName;
+
+        //Rotation
+        answerSaving.question = question;
+        AnswerSaving.lQuestion = answerSaving.question;
+
+        answerSaving.answer = answer;
+        AnswerSaving.lAnswer = answerSaving.answer;
+
+        answerSaving.answerCount+=1;
+        AnswerSaving.lAnswerCount = answerSaving.answerCount;
+        answerSaving.totalCount = AnswerSaving.lTotalCount;
+        string jsonData = JsonUtility.ToJson(answerSaving, true);
+        File.WriteAllText(jsonSavePath, jsonData);
+
+    }
+}
--- MedicalGameJam/Assets/Scripts/GameSaving/GameSaving.cs
+++ MedicalGameJam/Assets/Scripts/GameSaving/GameSaving.cs
@@ -1,14 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameSaving : MonoBehaviour
 {
+    public static GameSaving gameSaving;
+    private static string jsonSavePath ="";
+
+    public string sceneName;
+    public float score;
+
+    public static string lSceneName;
+    public static float lScore;
+
+    public static bool isLoaded = false;
+
+    private void Awake()
+    {
+        if(gameSaving == null)
+        {
+            gameSaving = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    public static void LoadData()
+    {
+        GameSaving gameSaving = JsonUtility.FromJson<GameSaving>(File.ReadAllText(Application.persistentDataPath + "/saveload.json"));
+
+        GameSaving.lSceneName = gameSaving.sceneName;
+        GameSaving.lScore = gameSaving.score;
+
+        SceneManager.LoadScene(gameSaving.sceneName);
+
+        //For testing purposes
+      
+        
+        GameSaving.isLoaded = true;
+    }
+
+    public static void SaveData(float score)
+    {
+        //References
+        Scene scene = SceneManager.GetActiveScene();
+        GameSaving gameSaving = new GameSaving();
+        //Scene Name
+        gameSaving.sceneName = scene.name;
+
+        //Position
+       
+        GameSaving.lSceneName = gameSaving.sceneName;
+
+        //Rotation
+        gameSaving.score = score;
+        GameSaving.lScore = gameSaving.score;
+
+        string jsonData = JsonUtility.ToJson(gameSaving, true);
+        File.WriteAllText(jsonSavePath, jsonData);
+
+    }
+
+
+
+
 
     // Update is called once per frame
     void Update()
